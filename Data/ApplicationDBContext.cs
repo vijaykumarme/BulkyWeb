01af1c8a5fd15caf50090865c916ab5d13002db9
vijaykumarme <vijaykdev19@gmail.com>
2023-10-27 23:57:03 +0530

    completed CRUD operations

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
